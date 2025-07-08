4dc7d34323740b5ae2f27fc3b5e56632ba23aa89
Vinicios Mendes <viniciosmendes908@gmail.com>
2025-07-07 23:04:04 -0300

    [+] paginas e templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
-import { Container } from "./Components/Container"
-import { Footer } from "./Components/Footer"
-import { InfoCard } from "./Components/InfoCard"
-import { Members } from "./Components/Members"
-import { MembersMap } from "./Components/MembersMap"
-import { Menu } from "./Components/Menu"
-import { Projects } from "./Components/Projects"
+import { Home } from "./pages/Home"
 
 function App() {
 
   return (
     <>
-      <Menu/>
-      <Container>
-
-        <InfoCard/>
-      </Container>
-<h1 className="text-3xl text-center mt-2">Integrantes do NCB</h1>
-      <h2 className="text-xl text-center">Nosso grupo é composto por alunos de diversos cursos, idades e estados do Brasil.</h2>
-
-    <Members />
-    <MembersMap />
-
-
-      <Projects/>
-      <Footer/>
+      <Home/>
       </>
   )
 }
