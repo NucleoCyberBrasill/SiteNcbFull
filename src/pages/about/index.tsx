da92eba8eeca34377dc5890fca6f2c458583a856
Vinicios Mendes <viniciosmendes908@gmail.com>
2025-07-24 20:48:13 -0300

    💄 template padrao na página sobre

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,11 @@
+import { MainTemplate } from "@/templates/MainTemplate";
+
 export function About(){
     return (
-        <>
+            <MainTemplate>
+        
         <h1 className="text-2xl text-center mt-2">Sobre o <span className="text-[var(--color-primary-self)]">NCB</span></h1>
         <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt quisquam molestiae neque totam error consectetur odio? Ipsam quo impedit neque repudiandae, odio dolorum velit? Sed error placeat neque quam maxime? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate hic minima quam excepturi! Vitae, nobis corrupti. Odit debitis itaque delectus odio, maxime asperiores id enim consequuntur, accusantium eius velit molestias.</p>
-        </>
+</MainTemplate>
     )
 }
