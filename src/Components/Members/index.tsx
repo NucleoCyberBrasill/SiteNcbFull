7b4678c2d7fddf786fc0413f31dc4dea6077e4ba
Bel <154826898+iambel@users.noreply.github.com>
2025-07-31 10:13:50 -0300

    :bug: fix: correção de imports e build

--- src/Components/Members/index.tsx
+++ src/Components/Members/index.tsx
@@ -11,7 +11,7 @@ import {
   CarouselItem,
   // CarouselNext,
   // CarouselPrevious,
-} from "@/components/ui/carousel";
+} from "@/Components/ui/carousel";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import membersData from "./members.json";
@@ -47,7 +47,7 @@ const tags: Record<TagKey, React.ReactNode> = {
   devops: <FaCogs title="DevOps" />,
   analytics: <FaChartBar title="Análise de dados" />,
   datascience: <FaChartLine title="Ciência de Dados" />,
-  designer: <FaPalette title="Design"/>
+  designer: <FaPalette title="Design" />,
 };
 
 export function Members() {
@@ -57,13 +57,13 @@ export function Members() {
 
   return (
     <section className="h-[70vh] flex justify-center">
-    <div className="max-w-5xl mx-auto rounded-sm px-4">
-      <Carousel className="cursor-grab px-5 ">
-        <CarouselContent className="flex p-3">
-          {membersData.map((member) => (
-            <CarouselItem 
-              key={member.id}
-              className="
+      <div className="max-w-5xl mx-auto rounded-sm px-4">
+        <Carousel className="cursor-grab px-5 ">
+          <CarouselContent className="flex p-3">
+            {membersData.map((member) => (
+              <CarouselItem
+                key={member.id}
+                className="
                 p-4
                 m-4
                 select-none
@@ -76,84 +76,84 @@ export function Members() {
                 hover:scale-106
                 hover:border-[var(--color-accent-hover-self)]
               "
-              data-aos="fade-up"
-            >
-              <img
-                className="
+                data-aos="fade-up"
+              >
+                <img
+                  className="
                   mt-2 h-32 w-32 
                   border-[var(--color-accent-hover-self)] border-5 
                   object-cover aspect-square 
                   rounded-full mx-auto mb-2
                 "
-                src={member.image}
-                alt={`Foto de perfil de ${member.name}`}
-              />
+                  src={member.image}
+                  alt={`Foto de perfil de ${member.name}`}
+                />
 
-              <h3
-                className="font-bold text-lg text-center"
-                dangerouslySetInnerHTML={{ __html: member.name }}
-              />
-              <div className="flex flex-wrap gap-2 items-center justify-center p-1">
-                {member.tags?.map((tag, index) => (
-                  <span
-                    key={index}
-                    className="px-2 py-1 rounded bg-[var(--color-secondary-self)] 
+                <h3
+                  className="font-bold text-lg text-center"
+                  dangerouslySetInnerHTML={{ __html: member.name }}
+                />
+                <div className="flex flex-wrap gap-2 items-center justify-center p-1">
+                  {member.tags?.map((tag, index) => (
+                    <span
+                      key={index}
+                      className="px-2 py-1 rounded bg-[var(--color-secondary-self)] 
                     hover:bg-[var(--color-secondary-hover-self)] text-xs text-[var( --color-text-self)] cursor-pointer"
-                  >
-                    {tags[tag as TagKey]}
-                  </span>
-                ))}
-              </div>
+                    >
+                      {tags[tag as TagKey]}
+                    </span>
+                  ))}
+                </div>
 
-              <p className="text-center text-sm mt-1 px-2">
-                {member.description}
-              </p>
+                <p className="text-center text-sm mt-1 px-2">
+                  {member.description}
+                </p>
 
-              <ul className="mt-3 flex items-center justify-center gap-3">
-                {member.github && (
-                  <li className="hover:scale-110 transition-transform">
-                    <a
-                      href={member.github}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label={`GitHub de ${member.name}`}
-                    >
-                      <FaGithub size={28} />
-                    </a>
-                  </li>
-                )}
-                {member.linkedin && (
-                  <li className="hover:scale-110 transition-transform">
-                    <a
-                      href={member.linkedin}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label={`LinkedIn de ${member.name}`}
-                    >
-                      <FaLinkedin size={28} />
-                    </a>
-                  </li>
-                )}
-                {member.discord && (
-                  <li className="hover:scale-110 transition-transform">
-                    <a
-                      href={member.discord}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      aria-label={`Discord de ${member.name}`}
-                    >
-                      <FaDiscord size={28} />
-                    </a>
-                  </li>
-                )}
-              </ul>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        {/* <CarouselPrevious className="bg-purple-600 text-white hover:bg-purple-700"/>
+                <ul className="mt-3 flex items-center justify-center gap-3">
+                  {member.github && (
+                    <li className="hover:scale-110 transition-transform">
+                      <a
+                        href={member.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={`GitHub de ${member.name}`}
+                      >
+                        <FaGithub size={28} />
+                      </a>
+                    </li>
+                  )}
+                  {member.linkedin && (
+                    <li className="hover:scale-110 transition-transform">
+                      <a
+                        href={member.linkedin}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={`LinkedIn de ${member.name}`}
+                      >
+                        <FaLinkedin size={28} />
+                      </a>
+                    </li>
+                  )}
+                  {member.discord && (
+                    <li className="hover:scale-110 transition-transform">
+                      <a
+                        href={member.discord}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label={`Discord de ${member.name}`}
+                      >
+                        <FaDiscord size={28} />
+                      </a>
+                    </li>
+                  )}
+                </ul>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          {/* <CarouselPrevious className="bg-purple-600 text-white hover:bg-purple-700"/>
         <CarouselNext className="bg-purple-600 text-white hover:bg-purple-700"/> */}
-      </Carousel>
-    </div>
+        </Carousel>
+      </div>
     </section>
   );
 }
