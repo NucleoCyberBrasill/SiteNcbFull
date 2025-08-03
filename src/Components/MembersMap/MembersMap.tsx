021d318682fdfffad5c25f105a137bb9b8db1063
Vinicios Mendes <viniciosmendes908@gmail.com>
2025-08-03 17:15:18 -0300

    :lipstick: style: animações no membersMap

--- src/Components/MembersMap/MembersMap.tsx
+++ src/Components/MembersMap/MembersMap.tsx
@@ -1,14 +1,22 @@
+import { useEffect } from "react";
+import AOS from "aos";
 import Map from "./Map";
 const MembersMap = () => {
+  useEffect(() => {
+    AOS.init({ once: false });
+  }, []);
   return (
-    <div className="h-auto mb-30 flex flex-col justify-center items-center">
+    <section
+      className="h-auto mb-30 flex flex-col justify-center items-center"
+      data-aos="fade-up"
+    >
       <h1 className="text-3xl text-center mt-2">Integrantes do NCB</h1>
       <h2 className="text-xl text-center">
         Nosso grupo é composto por alunos de diversos cursos, idades e estados
         do Brasil.
       </h2>
       <Map />
-    </div>
+    </section>
   );
 };
 
