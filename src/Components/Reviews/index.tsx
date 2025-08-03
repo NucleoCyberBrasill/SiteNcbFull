bdb233589ffa2065a9c894f6e24178817c8f0b74
Vinicios Mendes <viniciosmendes908@gmail.com>
2025-08-03 16:46:24 -0300

    :lipstick: style: reajuste no tamanho das reviews

--- src/Components/Reviews/index.tsx
+++ src/Components/Reviews/index.tsx
@@ -15,33 +15,35 @@ import {
 
 export function Reviews() {
   return (
-    <section className="m-2 mb-30 justify-center">
-            <div className="max-w-5xl mx-auto rounded-sm px-4">
-
-      <Carousel className="cursor-grab px-5 ">
-        <CarouselContent
-          className="p-2 m-2 gap-2
+    <section className="mb-30 justify-center">
+      <div className="max-w-5xl mx-auto rounded-sm">
+        <Carousel className="cursor-grab px-5 ">
+          <CarouselContent
+            className="p-2 gap-2
                 max-w-xs "
-        >
-          {reviews.map((review) => {
-            return (
-              <CarouselItem>
-                <Card className=" w-full max-w-sm mx-auto border border-[var(--color-primary-self)] bg-[var(--color-bg-self)] text-[var(--color-text-self)] hover:border-[var(--color-accent-hover-self)] hover:scale-106 select-none">
-                  <CardHeader className="flex">
-                    <img src={review.avatar} alt={`${review.name}+" avatar"`} />
-                    <CardTitle className="text-xl">{review.name}</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <CardDescription>{review.review}</CardDescription>
-                  </CardContent>
-                </Card>
-              </CarouselItem>
-            );
-          })}
-        </CarouselContent>
-        {/* <CarouselPrevious className="bg-purple-600 text-white hover:bg-purple-700" />
+          >
+            {reviews.map((review) => {
+              return (
+                <CarouselItem>
+                  <Card className="border border-[var(--color-primary-self)] bg-[var(--color-bg-self)] text-[var(--color-text-self)] hover:border-[var(--color-accent-hover-self)] hover:scale-106 select-none">
+                    <CardHeader className="flex">
+                      <img
+                        src={review.avatar}
+                        alt={`${review.name}+" avatar"`}
+                      />
+                      <CardTitle className="text-xl">{review.name}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                      <CardDescription>{review.review}</CardDescription>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              );
+            })}
+          </CarouselContent>
+          {/* <CarouselPrevious className="bg-purple-600 text-white hover:bg-purple-700" />
         <CarouselNext className="bg-purple-600 text-white hover:bg-purple-700" /> */}
-      </Carousel>
+        </Carousel>
       </div>
     </section>
   );
