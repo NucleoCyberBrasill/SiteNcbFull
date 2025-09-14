d2ce2beef5a07110c0c6b3882094e6544d8f582b
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-09-14 15:12:25 -0300

    :lipstick: style: Hero section
    alteração no tamanho dos textos e caracteres do DecriptedText alterado para binários

--- src/Components/features/Hero.tsx
+++ src/Components/features/Hero.tsx
@@ -4,22 +4,22 @@ export function Hero() {
   return (
     // hero section
     <section
-      className="min-h-[60vh] flex flex-col items-center justify-center text-center px-4"
+      className="min-h-[60vh] flex flex-col items-center justify-center text-left px-4"
       data-aos="fade-down"
     >
       <DecryptedText
         text="Núcleo Cyber Brasil"
         speed={100}
         maxIterations={20}
-  characters="01#@$%&*<>[]{}|/\\?!+-=█▓▒░"
-        className="text-5xl font-bold mb-4 text-[var(--color-primary-self)]"
+        characters="01"
+        className="text-7xl font-bold mb-4 text-[var(--color-primary-self)]"
         parentClassName="all-letters"
         encryptedClassName="text-5xl font-bold mb-4 text-green-700"
         animateOn="view"
         sequential
       />
       <TextType
-        className="text-xl max-w-2xl text-gray-600"
+        className="text-2xl max-w-2xl text-gray-600"
         typingSpeed={10}
         pauseDuration={1500}
         showCursor={true}
