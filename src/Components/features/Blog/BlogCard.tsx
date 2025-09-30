21bdb7c7440bbe22fe0992392745d17b470c2a62
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-09-30 13:34:31 -0300

    :bug: fix: responsividade no blog

--- src/Components/features/Blog/BlogCard.tsx
+++ src/Components/features/Blog/BlogCard.tsx
@@ -5,8 +5,8 @@ export function BlogCard({title, description, slug, banner}: post) {
     return (
         <div className="border p-2 rounded max-w-xl m-2">
                 <img className="rounded" src={banner} alt="" />
-                <h2 className="text-xl">{title}</h2>
-                <p>{description}</p>
+                <h2 className="text-2xl font-bold">{title}</h2>
+                <p className="break-words">{description}</p>
                 <a href={slug}><button className="flex gap-2">Ler post <Link2Icon /></button></a>
             </div>
     )
--- src/Components/features/Blog/index.tsx
+++ src/Components/features/Blog/index.tsx
@@ -4,7 +4,7 @@ import type { post } from '../../../types/post'
 
 export function BlogContent() {
     return (
-        <section className="h-screen justify-center items-center grid md:grid-cols-2 m-2">
+ <section className="justify-center items-center grid md:grid-cols-2 m-2 lg:grid-cols-3">
             {posts.map((p: post) => (
                 <BlogCard title={p.title} key={p.slug} description={p.description} slug={p.slug} banner={p.banner} />
             ))}
