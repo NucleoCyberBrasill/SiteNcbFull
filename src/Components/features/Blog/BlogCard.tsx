b69c0d3674cc0fafe16fba31108b14d414138efa
Vinícios Mendes <viniciosmendes908@gmail.com>
2025-09-30 14:10:16 -0300

    ✨ feat: data relativa dos posts

--- src/Components/features/Blog/BlogCard.tsx
+++ src/Components/features/Blog/BlogCard.tsx
@@ -1,13 +1,14 @@
 import { Link2Icon } from "lucide-react";
 import type { post } from "../../../types/post";
 import { formatDate } from "@/utils/formatDate";
+import { relativeDate } from "@/utils/relativeDate";
 
 export function BlogCard({ title, description, slug, banner, postedAt }: post) {
   return (
     <div className="border rounded p-2 m-2 max-w-sm sm:max-w-md md:max-w-lg">
       <img className="rounded w-full h-48 object-cover" src={banner} alt={title} />
       <h2 className="text-2xl font-bold mt-2">{title}</h2>
-      <p className="text-xs text-gray-500">{formatDate(postedAt, "dd/MM/yyyy 'às' HH:mm")}</p>
+      <p className="text-xs text-gray-500">{formatDate(postedAt, "dd/MM/yyyy 'às' HH:mm")} ({relativeDate(postedAt)})</p>
       <p className="break-words text-sm text-gray-300 line-clamp-3 mt-1">
         {description}
       </p>
